1b31270182fad08adde5b9b4577fadb5ea2f747e
Robin Schreiber <robin.schreiber@me.com>
2020-02-27 19:33:50 +0100

    update to es10

--- update.sh
+++ update.sh
@@ -9,5 +9,5 @@
 ./bin/estree-to-js.js es7Jsx --generate-json-spec --out generated/es7-jsx.json
 ./bin/estree-to-js.js es7Jsx --generate-visitor --json-spec generated/es7-jsx.json --out generated/es7-jsx-visitor.js
 
-./bin/estree-to-js.js es9 --generate-json-spec --out generated/es9.json
-./bin/estree-to-js.js es9 --generate-visitor --json-spec generated/es9.json --out generated/es9-visitor.js
+./bin/estree-to-js.js es10 --generate-json-spec --out generated/es10.json
+./bin/estree-to-js.js es10 --generate-visitor --json-spec generated/es10.json --out generated/es10-visitor.js --format esm
